056f743b7140d64467751f282f21eb0097e87c6f
artemhrebeniuk <146990956+artemhrebeniuk@users.noreply.github.com>
2023-11-19 14:12:52 +0200

    Update SetOperations.java

--- ArtemHrebeniuk/Example/src/main/java/homework/HM3/EX1/SetOperations.java
+++ ArtemHrebeniuk/Example/src/main/java/homework/HM3/EX1/SetOperations.java
@@ -1,9 +1,9 @@
-package hw_third.first_ex;
+package hm3_ex1;
 
 import java.util.HashSet;
 import java.util.Set;
 
-public class SetMethods {
+public class SetOperations {
 
     public Set<Integer> union(Set<Integer> set1, Set<Integer> set2) {
         Set<Integer> unionSet = new HashSet<>(set1);
