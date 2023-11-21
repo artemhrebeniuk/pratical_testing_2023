e89b8cdca25cdf975d8e9bc8a0feecab4252ca7f
artemhrebeniuk <146990956+artemhrebeniuk@users.noreply.github.com>
2023-11-21 11:48:41 +0200

    Update XPath.java

--- Team project/XPath.java
+++ Team project/XPath.java
@@ -3,3 +3,13 @@ xpath=//div[@id='app']/div/div/header/div/div/i
 xpath=//span[contains(.,'PIM')]
 xpath=//button[contains(.,' Add')]
 xpath=//button[contains(.,'Save')]
+
+
+interface ChangePassword {
+    String header = "//h2[text()='Change Password']";
+    String currentPasswordInput = "//input[@id='txtCurrentPassword']";
+    String newPasswordInput = "//input[@id='txtNewPassword']";
+    String confirmPasswordInput = "//input[@id='txtConfirmPassword']";
+    String saveButton = "//button[@type='submit']";
+}
+
