ed83b8c0ec5861b4b3eba4842911a4f403724dc8
artemhrebeniuk <146990956+artemhrebeniuk@users.noreply.github.com>
2023-11-21 11:52:33 +0200

    Update XPath.java

--- Team project/XPath.java
+++ Team project/XPath.java
@@ -1,6 +1,13 @@
 package constants;
 
 public class XPath {
+    public interface ChangePassword {
+        String header = "//h2[text()='Change Password']";
+        String currentPasswordInput = "//input[@id='txtCurrentPassword']";
+        String newPasswordInput = "//input[@id='txtNewPassword']";
+        String confirmPasswordInput = "//input[@id='txtConfirmPassword']";
+        String saveButton = "//button[@type='submit']";
+    }
 
     XPath for Adding new Employee
     xpath=//div[@id='app']/div/div/header/div/div/i
@@ -8,13 +15,5 @@ public class XPath {
     xpath=//button[contains(.,' Add')]
     xpath=//button[contains(.,'Save')]
 
-    public interface ChangePassword {
-        String header = "//h2[text()='Change Password']";
-        String currentPasswordInput = "//input[@id='txtCurrentPassword']";
-        String newPasswordInput = "//input[@id='txtNewPassword']";
-        String confirmPasswordInput = "//input[@id='txtConfirmPassword']";
-        String saveButton = "//button[@type='submit']";
-
-}
-
+    
 }
