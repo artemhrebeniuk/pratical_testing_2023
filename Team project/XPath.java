39e2a1c0e0b830366bfb12e42b90dbafe682281f
BilanDaria <131748468+BilanDaria@users.noreply.github.com>
2023-11-21 10:37:57 +0200

    Add XPath for new Employee XPath.java

--- Team project/XPath.java
+++ Team project/XPath.java
@@ -1,1 +1,5 @@
-
+XPath for Adding new Employee
+xpath=//div[@id='app']/div/div/header/div/div/i
+xpath=//span[contains(.,'PIM')]
+xpath=//button[contains(.,' Add')]
+xpath=//button[contains(.,'Save')]
