c25133de6c6cdca1e5d4df505b60ccfcefde6c25
Gold-Nick <146989873+Gold-Nick@users.noreply.github.com>
2023-11-23 00:39:14 +0300

    Update XPath.java

--- Team project/XPath.java
+++ Team project/XPath.java
@@ -18,6 +18,18 @@ public class XPath {
             thirdNameField = "//input[@name='lastName']",
             saveButton = "//button[contains(.,'Save')]";
         }
+    //Nick-Gold
+    public interface User_Login{
+        Login_Header = "//h5[text()='Login']";
+        Username_Button = "//button[@name='username']";
+        Password_Button = "//button[@name='password']";
+        Login_Button = "//button[@type='submit']";
+        Youtube_Link = "//a[@href='https://www.youtube.com/c/OrangeHRMInc']";
+        Facebook_Link = "//a[@href='https://www.facebook.com/OrangeHRM/']";
+        Linkedin_Link = "//a[@href='https://www.linkedin.com/company/orangehrm/mycompany/']";
+        Twitter_Link = "//a[@href='https://twitter.com/orangehrm?lang=en']";
+        Forgot_your_password_Header = "//p[text()='Forgot your password?']";
+    }
  
     
 }
